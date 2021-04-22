c4c6af4a3cc5093a7fa8292e6c790a5836d16b90
Nosovitski <nosovitski.gleb@gmail.com>
2021-04-22 21:35:05 +0300

    home

---
+++ DRAGNDROP/script.js
@@ -0,0 +1,47 @@
+"use strict"
+const container = document.querySelector(".container")
+// Если startMove === false, то обработчик событий mousemove работать не будет
+
+// Задаем разницу расстояний между курсором и началом мяча в глобальной области видимости.
+let shiftX = 0
+let shiftY = 0
+
+// Создаем 10 мячей, которые рандомно появятся в пределах контейнера 
+for (let i = 0; i < 10; i++) {
+	const div = document.createElement("div");
+	div.className = "ball"
+	div.style.left = `${Math.floor(Math.random() * (container.offsetWidth - 50))}px`
+	div.style.top =`${Math.floor(Math.random() * (container.offsetHeight - 50))}px`
+	container.appendChild(div)
+}
+
+const balls = document.querySelectorAll(".ball")
+
+function start(e) {
+	e.target.style.zIndex = 10
+	// Запоминаем разницу расстояний между курсором и началом мяча
+	shiftX = e.pageX - e.target.getBoundingClientRect().x
+	shiftY = e.pageY - e.target.getBoundingClientRect().y	
+	container.addEventListener("mousemove", move)
+}
+
+function move(e) {
+	if (e.target.classList.contains("container")) return
+	// Правильно позиционируем мяч
+	e.target.style.left = `${e.pageX - container.getBoundingClientRect().x - shiftX}px`;
+	e.target.style.top = `${e.pageY - container.getBoundingClientRect().y - shiftY}px`;
+}
+
+function end(e) {
+	e.target.style.zIndex = ""
+	e.target.removeEventListener("mousedown", start)
+	container.removeEventListener("mousemove", move)
+	e.target.addEventListener("mousedown", start)
+}
+
+balls.forEach((el) => {
+	el.addEventListener("mousedown", start)
+	el.addEventListener("mouseup", end)
+})
+
+	
